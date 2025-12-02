709293957d37942c4ce2e465518c699da4ab35d9
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-12-02 14:44:03 +0000

    Push all records to a single sequence

--- src/hashing/simdsketch_wrapper.rs
+++ src/hashing/simdsketch_wrapper.rs
@@ -32,35 +32,33 @@ pub fn sketch_with_simd(
         }
     }
 
-    let mut seqs: Vec<PackedNSeqVec> = vec![];
+    let mut seqs = PackedNSeqVec::default();
     let mut reader =
         parse_fastx_file(fastx1).unwrap_or_else(|_| panic!("Invalid path/file: {fastx1}"));
 
     if min_qual == 0 || !reads {
         while let Some(record) = reader.next() {
-            seqs.push(PackedNSeqVec::from_ascii(
-                &record.expect("Invalid FASTA/Q record").seq(),
-            ));
+            seqs.push_ascii(&record.expect("Invalid FASTA/Q record").seq());
+            seqs.push_ascii(b"N");
         }
 
         if fastx2.is_some() {
             let mut reader = parse_fastx_file(fastx2.as_ref().unwrap())
                 .unwrap_or_else(|_| panic!("Invalid path/file"));
             while let Some(record) = reader.next() {
-                seqs.push(PackedNSeqVec::from_ascii(
-                    &record.expect("Invalid FASTA/Q record").seq(),
-                ));
+                seqs.push_ascii(&record.expect("Invalid FASTA/Q record").seq());
+                seqs.push_ascii(b"N");
             }
         }
     } else {
         let min_qual_usize = min_qual as usize;
         while let Some(record) = reader.next() {
             let seqrec = record.expect("Invalid FASTA/Q record");
-            seqs.push(PackedNSeqVec::from_ascii_and_quality(
+            seqs.push_from_ascii_and_quality(
                 &seqrec.seq(),
                 &seqrec.qual().unwrap(),
                 min_qual_usize,
-            ));
+            );
         }
 
         if fastx2.is_some() {
@@ -68,21 +66,19 @@ pub fn sketch_with_simd(
                 .unwrap_or_else(|_| panic!("Invalid second path/file"));
             while let Some(record) = reader.next() {
                 let seqrec = record.expect("Invalid FASTA/Q record");
-                seqs.push(PackedNSeqVec::from_ascii_and_quality(
+                seqs.push_from_ascii_and_quality(
                     &seqrec.seq(),
                     &seqrec.qual().unwrap(),
                     min_qual_usize,
-                ));
+                );
             }
         }
     }
 
-    let seqslices = seqs.iter().map(|s| s.as_slice()).collect::<Vec<_>>();
-
     // Run the sketching // NOTE TODO: there is no filtering for the reads, apart from quality! I.e., the difference on counts is zero!
     let simdsketches = sketchers
         .iter()
-        .map(|is| is.build().sketch_seqs(&seqslices))
+        .map(|is| is.build().sketch(seqs.as_slice()))
         .collect::<Vec<_>>();
 
     // Get sketchlib.rust sketch objects
